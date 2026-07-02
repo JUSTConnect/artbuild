efe2706ad6784e1ec64674a345608c0eafdcb72b
bageus <36338626+bageus@users.noreply.github.com>
2026-07-02 13:47:10 +0200

    Wire validators into tools registry

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,7 +1,12 @@
+import { validateBuildingCatalog, validateBuildingDefinition } from "./validators/building-definition-validator.js";
+
+export { validateBuildingCatalog, validateBuildingDefinition };
+
 export const toolModules = Object.freeze([
   Object.freeze({
     name: "tools/validators",
-    responsibility: "Validates content data, placement anchors and balance assumptions."
+    responsibility: "Validates content data, placement anchors and balance assumptions.",
+    validators: ["building-definition-validator"]
   }),
   Object.freeze({
     name: "tools/prototypes",
