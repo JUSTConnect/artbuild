079010f76d0817f675882e79d5732737640fa5ce
bageus <36338626+bageus@users.noreply.github.com>
2026-07-02 13:46:00 +0200

    Add building taxonomy constants

---
+++ src/content/rules/building-taxonomy.js
@@ -0,0 +1,39 @@
+export const buildingCategories = Object.freeze([
+  "Residential",
+  "Recreation",
+  "Knowledge",
+  "Technology",
+  "Art",
+  "Commerce",
+  "Energy",
+  "Decor",
+  "Foundation"
+]);
+
+export const connectionTypes = Object.freeze([
+  "Top",
+  "SideLeft",
+  "SideRight",
+  "Bridge",
+  "Roof",
+  "Utility",
+  "Decor"
+]);
+
+export const functionOptions = Object.freeze([
+  "Housing",
+  "Recreation",
+  "Knowledge",
+  "Technology",
+  "Art",
+  "Commerce",
+  "Energy",
+  "Decor",
+  "Foundation"
+]);
+
+export const rarityLevels = Object.freeze([
+  "Common",
+  "Uncommon",
+  "Rare"
+]);
